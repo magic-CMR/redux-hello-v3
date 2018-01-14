5b2c1810b794dbc8f04f2894882f87874bfe8b39
magic-C-M-R <1941600049@qq.com>
2018-01-14 17:49:27 +0800

    评论state存入store

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const initialState = [
+  {
+    id:'asf123',
+    text:'hello'
+  },
+  {
+    id:'zxc123',
+    text:'hi'
+  }
+]
+
+const rootReducer = (state = initialState, action) => {
+  return state
+}
+
+export default rootReducer
